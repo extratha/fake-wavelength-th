232a9477d87af6fe071d8ff7e9756675db17e355
ThakornT <thakorn.t@codemonday.com>
2025-07-25 11:57:10 +0700

    feat: update configuration and enhance WheelDial component with improved image handling and code formatting

--- src/screen/Main/GameContent/WheelDial/index.tsx
+++ src/screen/Main/GameContent/WheelDial/index.tsx
@@ -1,43 +1,43 @@
-import { useEffect, useRef, useState } from 'react';
-import clsx from 'clsx';
-import { socket } from '@/lib/socket'
-import { GameState } from '..';
-import { useUserProfile } from '@/hooks/useUserProfile';
-import Image from 'next/image';
+import { useEffect, useRef, useState } from "react";
+import clsx from "clsx";
+import { socket } from "@/lib/socket";
+import { GameState } from "..";
+import { useUserProfile } from "@/hooks/useUserProfile";
+import Image from "next/image";
 
-import ImageWheelScore from '@/assets/wheelBGnum.png'
-import ImageWheelScreen from '@/assets/wheelScreen.png'
-import ImageWheelDial from '@/assets/wheelDial.png'
-import ImageChromeBasic from '@/assets/chromeBasic.png'
-import Modal from '@/component/Modal';
+// import ImageWheelScore from "../../../../../public/wheelBGnum.png";
+// import ImageWheelScreen from "../../../../assets/wheelScreen.png";
+// import ImageWheelDial from "../../../../assets/wheelDial.png";
+// import ImageChromeBasic from "../../../../assets/chromeBasic.png";
+import Modal from "@/component/Modal";
 
 type WheelDialProps = {
-  gameState: GameState,
-}
+  gameState: GameState;
+};
 
 const WheelDial = ({ gameState }: WheelDialProps) => {
-  const { profile } = useUserProfile()
+  const { profile } = useUserProfile();
 
   const [modalOptions, setModalOptions] = useState({
     open: false,
-  })
-  const [isShowWheelMarker, setIsShowWheelMarker] = useState(false)
-  const [peekScreen, setIsPeekScreen] = useState(false)
-  const [wheelHeight, setWheelHeight] = useState('')
-  const wheelWrapRef = useRef<HTMLDivElement | null>(null)
-  const wheelControl = useRef<HTMLDivElement | null>(null)
+  });
+  const [isShowWheelMarker, setIsShowWheelMarker] = useState(false);
+  const [peekScreen, setIsPeekScreen] = useState(false);
+  const [wheelHeight, setWheelHeight] = useState("");
+  const wheelWrapRef = useRef<HTMLDivElement | null>(null);
+  const wheelControl = useRef<HTMLDivElement | null>(null);
 
-  const wheelScreen = document.getElementById('wheelScreen')
-  const isClueGiver = gameState.clueGiver === profile.userId
-  const isHost = gameState.hostId === profile.userId
+  const wheelScreen = document.getElementById("wheelScreen");
+  const isClueGiver = gameState.clueGiver === profile.userId;
+  const isHost = gameState.hostId === profile.userId;
 
   const rotateDial = (deg: number) => {
-    if (deg > 0 && gameState.dialRotation >= 90) return
-    if (deg < 0 && gameState.dialRotation <= -90) return
+    if (deg > 0 && gameState.dialRotation >= 90) return;
+    if (deg < 0 && gameState.dialRotation <= -90) return;
 
     const newRotation = gameState.dialRotation + deg;
 
-    socket.emit('updateDialRotation', {
+    socket.emit("updateDialRotation", {
       roomId: gameState.roomId,
       rotation: newRotation,
       userName: profile.userName,
@@ -46,20 +46,20 @@ const WheelDial = ({ gameState }: WheelDialProps) => {
 
   const toggleScreen = () => {
     if (gameState.screenOpen) {
-      socket.emit('toggleScreen', {
+      socket.emit("toggleScreen", {
         roomId: gameState.roomId,
         screenOpen: false,
         userName: profile.userName,
       });
-      return
+      return;
     }
     setModalOptions({
       open: true,
-    })
+    });
   };
 
   const handleConfirmToggleScreen = () => {
-    socket.emit('toggleScreen', {
+    socket.emit("toggleScreen", {
       roomId: gameState.roomId,
       screenOpen: true,
       userName: profile.userName,
@@ -67,12 +67,12 @@ const WheelDial = ({ gameState }: WheelDialProps) => {
 
     setModalOptions({
       open: false,
-    })
-  }
+    });
+  };
 
   const randomizeMarker = () => {
     const randDeg = Math.floor(Math.random() * 180) - 90;
-    socket.emit('randomizeMarker', {
+    socket.emit("randomizeMarker", {
       roomId: gameState.roomId,
       rotation: randDeg,
       userName: profile.userName,
@@ -80,41 +80,50 @@ const WheelDial = ({ gameState }: WheelDialProps) => {
   };
 
   const handlePeekScreen = () => {
-    setIsPeekScreen(!peekScreen)
-  }
+    setIsPeekScreen(!peekScreen);
+  };
 
   useEffect(() => {
-    if (wheelScreen) setIsShowWheelMarker(wheelScreen && wheelHeight ? true : false)
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [wheelScreen])
+    if (wheelScreen)
+      setIsShowWheelMarker(wheelScreen && wheelHeight ? true : false);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [wheelScreen]);
 
   useEffect(() => {
     const updateHeight = () => {
       if (wheelWrapRef.current) {
-        console.log(wheelWrapRef.current.clientHeight / 2, wheelWrapRef.current.clientWidth)
+        console.log(
+          wheelWrapRef.current.clientHeight / 2,
+          wheelWrapRef.current.clientWidth
+        );
         if (wheelWrapRef.current.clientWidth > 250) {
-          setWheelHeight(`${wheelWrapRef.current.clientHeight / 2}px`)
-        }else {
-          setWheelHeight('100%')
+          setWheelHeight(`${wheelWrapRef.current.clientHeight / 2}px`);
+        } else {
+          setWheelHeight("100%");
         }
       }
-    }
+    };
 
-    updateHeight()
+    updateHeight();
 
-    window.addEventListener('resize', updateHeight)
+    window.addEventListener("resize", updateHeight);
     return () => {
-      window.removeEventListener('resize', updateHeight)
-    }
-  }, [])
+      window.removeEventListener("resize", updateHeight);
+    };
+  }, []);
 
   return (
     <div className="relative w-full p2">
-      <div id="wheelWrap" ref={wheelWrapRef} className="relative w-[calc(100%-100px)] max-w-[1200px] aspect-square mx-auto ">
-
-        <div id="wheel" className="relative w-full overflow-hidden border-4 border-[#4b352a]"
+      <div
+        id="wheelWrap"
+        ref={wheelWrapRef}
+        className="relative w-[calc(100%-100px)] max-w-[1200px] aspect-square mx-auto "
+      >
+        <div
+          id="wheel"
+          className="relative w-full overflow-hidden border-4 border-[#4b352a]"
           style={{
-            height: wheelHeight
+            height: wheelHeight,
           }}
         >
           {/* <div className="absolute left-0 top-[-8px] w-full h-3  bg-darkBrown"
@@ -122,15 +131,28 @@ const WheelDial = ({ gameState }: WheelDialProps) => {
           /> */}
 
           {/* Wheel Frame */}
-          <div className="absolute left-0  w-full"
+          <div
+            className="absolute left-0  w-full"
             style={{
               zIndex: 11,
             }}
           >
-            <Image src={ImageChromeBasic} alt=""></Image>
+            <Image
+              src={
+                "https://res.cloudinary.com/dpya79wdj/image/upload/v1753419058/chromeBasic_dvojai.png"
+              }
+              alt=""
+              width={0}
+              height={0}
+              sizes="100vw"
+              className="w-full h-auto"
+            ></Image>
           </div>
-          <div id="hider" ref={wheelWrapRef} className="absolute bottom-[-2px] left-0 w-full h-[5px] bg-darkBrown "
-            style={{ transform: 'translateY(-1px)', zIndex: 11 }}
+          <div
+            id="hider"
+            ref={wheelWrapRef}
+            className="absolute bottom-[-2px] left-0 w-full h-[5px] bg-darkBrown "
+            style={{ transform: "translateY(-1px)", zIndex: 11 }}
           />
 
           {/* Wheel Screen */}
@@ -143,49 +165,114 @@ const WheelDial = ({ gameState }: WheelDialProps) => {
             )}
             style={{ zIndex: 5 }}
           >
-            <Image src={ImageWheelScreen} alt=""></Image>
+            <Image
+              src={
+                "https://res.cloudinary.com/dpya79wdj/image/upload/v1753419059/wheelScreen_nttzdb.png"
+              }
+              alt=""
+              width={0}
+              height={0}
+              sizes="100vw"
+              className="w-full h-auto"
+            ></Image>
           </div>
 
           {/* Wheel Marker */}
-          {isShowWheelMarker && <div
-            className="absolute top-0 left-0 w-full  p-1 flex items-center justify-center z-1 scale-[0.9]"
-            style={{ transform: `rotate(${gameState.markerRotation}deg)`,zIndex:1 }}
-          >
-            <Image src={ImageWheelScore} alt="" ></Image>
-          </div>}
+          {isShowWheelMarker && (
+            <div
+              className="absolute top-0 left-0 w-full  p-1 flex items-center justify-center z-1 scale-[0.9]"
+              style={{
+                transform: `rotate(${gameState.markerRotation}deg)`,
+                zIndex: 1,
+              }}
+            >
+              <Image
+                src={
+                  "https://res.cloudinary.com/dpya79wdj/image/upload/v1753418311/wheelBGnum_rcrfvd.png"
+                }
+                alt=""
+                width={0}
+                height={0}
+                sizes="100vw"
+                className="w-full h-auto"
+              ></Image>
+            </div>
+          )}
 
           {/* Wheel Dial */}
           <div
             className="absolute top-0 left-0 w-full z-10 transition-transform duration-300 "
-            style={{ transform: `rotate(${gameState.dialRotation}deg)`, scale: 2 }}
+            style={{
+              transform: `rotate(${gameState.dialRotation}deg)`,
+              scale: 2,
+            }}
           >
-            <Image src={ImageWheelDial} alt=""></Image>
+            <Image
+              src={
+                "https://res.cloudinary.com/dpya79wdj/image/upload/v1753419058/wheelDial_xpfqxq.png"
+              }
+              alt=""
+              width={0}
+              height={0}
+              sizes="100vw"
+              className="w-full h-auto"
+            ></Image>
           </div>
         </div>
 
-
         {/* Controls */}
-        <div ref={wheelControl} className=" w-full z-20 left-0 sm:mt-20 mx-auto" >
-          {
-            isClueGiver && <div className="w-full  top-[50%] left-[-36%] sm:left-0 mx-auto flex justify-center z-50">
-              <button onClick={handlePeekScreen} className="w-20 h-20  px-3 py-1 bg-lightYellow rounded-[300px] text-darkBrown font-medium">
+        <div
+          ref={wheelControl}
+          className=" w-full z-20 left-0 sm:mt-20 mx-auto"
+        >
+          {isClueGiver && (
+            <div className="w-full  top-[50%] left-[-36%] sm:left-0 mx-auto flex justify-center z-50">
+              <button
+                onClick={handlePeekScreen}
+                className="w-20 h-20  px-3 py-1 bg-lightYellow rounded-[300px] text-darkBrown font-medium"
+              >
                 {peekScreen ? "ซ่อนคะแนน" : "แง้มดูคะแนน"}
               </button>
             </div>
-          }
+          )}
 
-          <div className="w-full flex flex-col sm:flex-row justify-center items-center bottom-[100px] left-0 p-5 gap-6 sm:gap-3 mx-auto text-darkBrown"
-            style={{ zIndex: '10' }}
+          <div
+            className="w-full flex flex-col sm:flex-row justify-center items-center bottom-[100px] left-0 p-5 gap-6 sm:gap-3 mx-auto text-darkBrown"
+            style={{ zIndex: "10" }}
           >
-            {(isClueGiver || isHost) &&
-              <button onClick={randomizeMarker} className="h-10 px-3 py-1 bg-lightYellow rounded-lg max-w-40 font-medium ">สุ่มหมุนคะแนน</button>
-
-            }
+            {(isClueGiver || isHost) && (
+              <button
+                onClick={randomizeMarker}
+                className="h-10 px-3 py-1 bg-lightYellow rounded-lg max-w-40 font-medium "
+              >
+                สุ่มหมุนคะแนน
+              </button>
+            )}
             <div className="flex gap-2 items-center">
-              <button onClick={() => rotateDial(-10)} className="w-10 h-10 px-3 py-1 bg-lightYellow rounded-[50px]">-</button>
-              <button onClick={() => rotateDial(-1)} className="w-8 h-8 px-3 py-1 bg-lightYellow rounded-[50px]">-</button>
-              <button onClick={() => rotateDial(1)} className="w-8 h-8 px-3 py-1 bg-lightYellow rounded-[50px] font-medium">+</button>
-              <button onClick={() => rotateDial(10)} className="w-10 h-10 px-3 py-1 bg-lightYellow rounded-[50px] font-medium">+</button>
+              <button
+                onClick={() => rotateDial(-10)}
+                className="w-10 h-10 px-3 py-1 bg-lightYellow rounded-[50px]"
+              >
+                -
+              </button>
+              <button
+                onClick={() => rotateDial(-1)}
+                className="w-8 h-8 px-3 py-1 bg-lightYellow rounded-[50px]"
+              >
+                -
+              </button>
+              <button
+                onClick={() => rotateDial(1)}
+                className="w-8 h-8 px-3 py-1 bg-lightYellow rounded-[50px] font-medium"
+              >
+                +
+              </button>
+              <button
+                onClick={() => rotateDial(10)}
+                className="w-10 h-10 px-3 py-1 bg-lightYellow rounded-[50px] font-medium"
+              >
+                +
+              </button>
             </div>
 
             {(isClueGiver || isHost) && (
@@ -193,32 +280,41 @@ const WheelDial = ({ gameState }: WheelDialProps) => {
                 onClick={toggleScreen}
                 className="animated-border-button h-10 px-3 py-1 max-w-40 font-medium"
               >
-                <p>{gameState?.screenOpen ? "ซ่อนคะแนน" : "เปิดคะแนนให้ทุกคน"}</p>
+                <p>
+                  {gameState?.screenOpen ? "ซ่อนคะแนน" : "เปิดคะแนนให้ทุกคน"}
+                </p>
               </button>
             )}
           </div>
         </div>
       </div>
 
-
       <Modal options={modalOptions}>
         <div>
-          <p className="text-[20px] font-medium text-center">ยืนยันเปิดคะแนนหรือไม่</p>
+          <p className="text-[20px] font-medium text-center">
+            ยืนยันเปิดคะแนนหรือไม่
+          </p>
           <div className="flex flex-row justify-between mt-4">
-            <button className="w-20 rounded-lg bg-darkBrown text-white p-2" onClick={handleConfirmToggleScreen}>ยืนยัน</button>
+            <button
+              className="w-20 rounded-lg bg-darkBrown text-white p-2"
+              onClick={handleConfirmToggleScreen}
+            >
+              ยืนยัน
+            </button>
             <button
               className="w-20 rounded-lg bg-darkBrown text-white p-2 "
               onClick={() => {
                 setModalOptions({
                   open: false,
-                })
-              }}>ปิด</button>
-
+                });
+              }}
+            >
+              ปิด
+            </button>
           </div>
         </div>
       </Modal>
     </div>
-
   );
 };
 
